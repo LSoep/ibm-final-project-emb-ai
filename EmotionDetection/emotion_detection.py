8dbc9b373b05fbf6b0c3f28a04443036b8c1c882
LSoep <leila.soep@gmail.com>
2024-08-05 14:40:03 -0400

    test file

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -13,34 +13,7 @@ def emotion_detector(text_to_analyze):
 
     response = requests.post(url, json = input_text, headers = headers)
 
-    json_reponse = json.loads(response.text)
-    anger_value = json_reponse['emotionPredictions'][0]['emotion']['anger']
-    disgust_value = json_reponse['emotionPredictions'][0]['emotion']['disgust']
-    fear_value = json_reponse['emotionPredictions'][0]['emotion']['fear']
-    joy_value = json_reponse['emotionPredictions'][0]['emotion']['joy']
-    sadness_value = json_reponse['emotionPredictions'][0]['emotion']['sadness']
+    if response.status_code == 500:
+        return "Error"
 
-    dominant_emotion = "anger"
-    dominant_value = anger_value
-
-    if disgust_value > dominant_value:
-        dominant_emotion = "disgust"
-        dominant_value = disgust_value
-    if fear_value > dominant_value:
-        dominant_emotion = "fear"
-        dominant_value = fear_value
-    if joy_value > dominant_value:
-        dominant_emotion = "joy"
-        dominant_value = joy_value
-    if sadness_value > dominant_value:
-        dominant_emotion = "sadness"
-        dominant_value = sadness_value
-
-    return {
-    'anger': anger_value,
-    'disgust': disgust_value,
-    'fear': fear_value,
-    'joy': joy_value,
-    'sadness': sadness_value,
-    'dominant_emotion': dominant_emotion
-    }
+    
